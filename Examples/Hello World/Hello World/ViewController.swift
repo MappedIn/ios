4cda6ef83ec8078c938fd36bbf1b5351e60b67a0
Paul Bernhardt <paul@mappedin.ca>
2016-01-29 18:16:01 -0500

    Setup project skeleton

---
+++ Examples/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Paul Bernhardt on 2016-01-29.
+//  Copyright © 2016 MappedIn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Examples/Hello World/Hello WorldTests/Hello_WorldTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hello_WorldTests.swift
+//  Hello WorldTests
+//
+//  Created by Paul Bernhardt on 2016-01-29.
+//  Copyright © 2016 MappedIn. All rights reserved.
+//
+
+import XCTest
+@testable import Hello_World
+
+class Hello_WorldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
