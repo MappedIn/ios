bf30d7aea110a5f18aabf6d4db800955d8cc907f
Paul Bernhardt <paul@mappedin.ca>
2016-02-02 11:35:10 -0500

    Added error message into the Hello World app itself, and documented a bit more thoroughly.

--- Examples/Hello World/Hello World/ViewController.swift
+++ Examples/Hello World/Hello World/ViewController.swift
@@ -9,30 +9,44 @@
 import UIKit
 import MappedIn
 
+/**
+This is a dead-simple example designed to let you make sure you have valid credentials, a venue, and can build with the SDK.
+ 
+ All you need to do is modify the Info.plist file to include your MappedInUsername and MappedInPassword, both of which should have been provided by MappedIn. Then hit run, and after it loads you should see a map displayed on screen.
+*/
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MapView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     var venue = [Venue]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Get a list of all venues your credentials can access, and then pass it to a function that loads the full details of the first one.
         MappedIn.getVenues(loadFirstVenue)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: Functions
+    /// Load the full details (Maps, Locations, etc) for the first Venue we find)
     func loadFirstVenue(venues: [Venue]) {
         if let venue: Venue = venues.first {
             MappedIn.getVenue(venue, callback: displayVenue)
         } else {
-            print("No venues found. Make sure you set your MappedInUsername and MappedInPassword in your info.plist. If you did, talk to your MappedIn representative to ensure your key has access to a venue");
+            // Send this back to the main thread to do UI work
+            dispatch_async(dispatch_get_main_queue()) {
+                let errorMessage = "No venues found. Make sure you set your MappedInUsername and MappedInPassword in your info.plist. If you did, talk to your MappedIn representative to ensure your key has access to a venue"
+                self.errorLabel.text = errorMessage
+                self.errorLabel.enabled = true
+                self.errorLabel.hidden = false
+                print(errorMessage);
+                self.mapView.hidden = true
+            }
         }
     }
     
+    /// Tells the MapView to display a map for a certain venue
     func displayVenue(venue: Venue) {
         print("Venue got")
         mapView.venue = venue
