480323275d15e25485f933b017aef5cf2e4ca9b9
Paul Bernhardt <paul@mappedin.ca>
2016-02-03 17:09:33 -0500

    Basics of full Single Venue demo

--- Examples/Single Venue/Single Venue/ViewController.swift
+++ Examples/Single Venue/Single Venue/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MappedIn
+import Foundation
 
 /**
  This is a dead-simple example designed to let you make sure you have valid credentials, a venue, and can build with the SDK.
@@ -19,11 +20,51 @@ class ViewController: UIViewController, MapViewDelegate {
     @IBOutlet weak var mapView: MapView!
     @IBOutlet weak var errorLabel: UILabel!
     
+    @IBOutlet weak var mapStepper: UIStepper!
     
-    var venue = [Venue]()
+    //MARK: Directions controls
+    @IBOutlet weak var directionsView: UIView!
+    @IBOutlet weak var directionsStepper: UIStepper!
+    
+    //MARK: Details controls
+    @IBOutlet weak var locationImageView: UIImageView!
+    @IBOutlet var locationTitleLabel: UILabel!
+    @IBOutlet var locationDescriptionLabel: UILabel!
+    @IBOutlet var navigateToLocationButton: UIButton!
+    
+    @IBOutlet var instructionsLabel: UILabel!
+    
+    @IBAction func navigateToLocationButtonTapped(sender: UIButton) {
+        directionsView.hidden = false
+        if currentlySelectedPolygon?.entrances.count > 0 {
+            instructionsLabel.text = "Select origin"
+            directionsDestinationPolygon = currentlySelectedPolygon
+        } else {
+            instructionsLabel.text = "This location has no entrances, select another"
+        }
+        
+        
+    }
+    
+    @IBOutlet weak var detailsView: UIView!
+    
+    @IBAction func mapStepperChanged(sender: UIStepper) {
+        mapView.changeMap(Int(sender.value))
+    }
+    
+    @IBAction func directionsStepperChanged(sender: AnyObject) {
+        let index = Int(directionsStepper.value)
+        mapView.highlightNode(directions!.path, index: index)
+        instructionsLabel.text = directions!.directions[index].instruction
+    }
+    
+    private var directionsDestinationPolygon: Polygon?
+    private var currentlySelectedPolygon: Polygon?
+    private var directions: Directions?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         // Get a list of all venues your credentials can access, and then pass it to a function that loads the full details of the first one.
         MappedIn.getVenues(loadFirstVenue)
     }
@@ -34,46 +75,122 @@ class ViewController: UIViewController, MapViewDelegate {
         if let venue: Venue = venues.first {
             MappedIn.getVenue(venue, callback: displayVenue)
         } else {
-            // Send this back to the main thread to do UI work
-            dispatch_async(dispatch_get_main_queue()) {
-                let errorMessage = "No venues found. Make sure you set your MappedInUsername and MappedInPassword in your info.plist. If you did, talk to your MappedIn representative to ensure your key has access to a venue"
-                self.errorLabel.text = errorMessage
-                self.errorLabel.enabled = true
-                self.errorLabel.hidden = false
-                print(errorMessage);
-                self.mapView.hidden = true
-            }
+            logError("No venues found. Make sure you set your MappedInUsername and MappedInPassword in your info.plist. If you did, talk to your MappedIn representative to ensure your key has access to a venue")
         }
     }
     
     /// Tells the MapView to display a map for a certain venue
     func displayVenue(venue: Venue) {
-        print("Venue got")
+        print("Venue loaded, displaying")
         mapView.venue = venue
         mapView.loadScene()
+        clearLocationInformation()
+    }
+    
+    func logError(errorMessage: String) {
+        // Send this back to the main thread to do UI work
+        dispatch_async(dispatch_get_main_queue()) {
+            
+            self.errorLabel.text = errorMessage
+            self.errorLabel.enabled = true
+            self.errorLabel.hidden = false
+            print(errorMessage);
+            self.mapView.hidden = true
+        }
+    }
+    
+    func displayLocationInformation(location: Location) {
+
+        if let image = loadImageFromURL(location.picture?[150]) {
+            locationImageView.image = image
+        }
+        locationTitleLabel.text = location.name
+        locationDescriptionLabel.text = location.description
+        navigateToLocationButton.hidden = false
+        if directions == nil {
+            instructionsLabel.text = ""
+        }
+    }
+    
+    func clearLocationInformation() {
+        if let image = loadImageFromURL(mapView.venue?.logo?[150]) {
+           locationImageView.image = image
+        }
+        
+        locationTitleLabel.text = mapView.venue?.name
+        locationDescriptionLabel.text = "Select a location"
+        navigateToLocationButton.hidden = true
+    }
+    
+    func clearDirections() {
+        directionsView.hidden = true
+        directions = nil
+        directionsDestinationPolygon = nil
+    }
+    
+    func loadImageFromURL(url: NSURL?) -> UIImage? {
+        if url == nil {
+            return UIImage()
+        }
+        if let imageData = NSData(contentsOfURL: url!) {
+            return UIImage(data: imageData)
+        }
+        return UIImage()
     }
     
     // MARK: MapViewDelegate
     /// Since data is being pulled down from the Internet, this is called when the scene is finished loading and ready for use.
     func sceneLoaded() {
         print("Scene loaded!")
-    }
-    
-    /// Called when a user taps on a polygon in the MapView and it belongs to one or more locations
-    /// Parameter location: The Location(s) associated with the polygon. Often one, but could depend on the map
-    func locationSelected(location: [Location]){
+        let mapCount = mapView.venue?.maps?.count
+        if mapCount > 0 {
+            mapStepper.maximumValue = Double(mapCount!)
+        } else {
+            logError("Something horrible has happened, because your venue has no maps. Check your internet connection, or contacted your MappedIn representative")
+        }
         
     }
     
     /// Called when a user taps a specific polygon
     /// Parameter polygon: the Polygon the user tapped on
-    func polygonSelected(polygon: Polygon) {
+    func polygonTapped(polygon: Polygon) {
         
+        
+        mapView.clearHighlightedPolygons()
+        if polygon.locations.count > 0 {
+            currentlySelectedPolygon = polygon
+            mapView.highlightPolygon(polygon)
+        
+            if directionsDestinationPolygon != nil {
+                if polygon.entrances.count > 0 {
+                
+                    directions = polygon.entrances.directionsTo(directionsDestinationPolygon!.entrances)
+                    if let path = directions?.path {
+                        mapView.drawPath(path)
+                        mapView.highlightNode(path, index: 0)
+                        instructionsLabel.text = directions!.directions[0].instruction
+                        directionsStepper.maximumValue = Double(path.count)
+                        directionsStepper.value = 0
+                    } else {
+                        locationTitleLabel.text = "No path found."
+                    }
+                    
+                } else {
+                    locationTitleLabel.text = "Location has no entrances. Select another."
+                }
+            
+            } else {
+            
+                displayLocationInformation(polygon.locations[0])
+            
+            }
+        }
     }
     
     /// Called when the user cleared their location selction. Useful to let you clear any aditional information you were displaying for the location
-    func locationsCleared() {
-        
+    func nothingTapped() {
+        clearLocationInformation()
+        clearDirections()
     }
     
     /// Called when the user has started moving the map around
