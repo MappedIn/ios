c5bda0c390b3502ab07323f22275ceca0ea118cc
christiefelker <christie.felker@mappedin.ca>
2019-06-05 16:33:40 -0400

    Add webview demo to ios external demos and update readme

---
+++ Examples/WebView-Sample/WebView-Sample/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  A short exmaple of displaying Mappedin Web in a WebView.
+//
+//  Copyright © 2019 Mappedin. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    @IBOutlet var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        view = webView
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Setup WebView with local html file. Another option is to use an external url and load it here.
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "Website") {
+            webView.loadFileURL(url, allowingReadAccessTo: url)
+            let myRequest = URLRequest(url: url)
+            webView.load(myRequest)
+        } else {
+            print("Website files not found")
+        }
+        
+        
+    }
+    
+    //Setup in order to open certain links in an external browser
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url {
+            /*
+            Logic to decide which urls will be loaded in an external browser.
+            Because we are loading from a local file, any url starting with http would be external websites
+            */
+            if url.absoluteString.hasPrefix("http") {
+                UIApplication.shared.open(url)
+                decisionHandler(.cancel)
+                return
+            }
+        }
+        decisionHandler(.allow)
+    }
+}
+
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 This repo contains a simple example to get you started with the MappedIn iOS SDK. Before you can do anything, make sure you have an API key from MappedIn. Talk to your representative to get one.
 
 You can view all the up to date documentation in this project's [GitHub Page](http://mappedin.github.io/ios/).
+
+
+#Mappedin iOS WebView Example
+
+This is a short demonstration of creating a Web View in an iOS application, and displaying Mappedin Web. It also contains an example of setting up external links to open in a separate browser. 
+
+This example requires you to have a Mappedin Web API key. Please talk to your representative if you need one. 
