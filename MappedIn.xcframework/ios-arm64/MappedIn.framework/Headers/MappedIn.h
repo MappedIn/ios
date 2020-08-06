f52abe6590489a14490330203df476c5218abd5c
youel <youel.kaisar@mappedin.ca>
2020-08-06 10:52:56 -0400

    Added xcframework for sdk v2

---
+++ MappedIn.xcframework/ios-arm64/MappedIn.framework/Headers/MappedIn.h
@@ -0,0 +1,19 @@
+//
+//  MappedIn.h
+//  MappedIn
+//
+//  Created by Sean Lin on 2020-05-12.
+//  Copyright © 2020 Mappedin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MappedIn.
+FOUNDATION_EXPORT double MappedInVersionNumber;
+
+//! Project version string for MappedIn.
+FOUNDATION_EXPORT const unsigned char MappedInVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MappedIn/PublicHeader.h>
+
+
---
+++ MappedIn.xcframework/ios-x86_64-simulator/MappedIn.framework/Frameworks/MappedinVenueFormat.framework/Headers/MappedinVenueFormat.h
@@ -0,0 +1,19 @@
+//
+//  MappedinVenueFormat.h
+//  MappedinVenueFormat
+//
+//  Created by Ed Wei on 10/24/19.
+//  Copyright © 2019 Mappedin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MappedinVenueFormat
+FOUNDATION_EXPORT double MappedinVenueFormatVersionNumber;
+
+//! Project version string for MappedinVenueFormat.
+FOUNDATION_EXPORT const unsigned char MappedinVenueFormatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVFDownloader/PublicHeader.h>
+
+
