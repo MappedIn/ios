74f237e48c9061db8c36f6f444343af9fc520785
youel <youel.kaisar@mappedin.ca>
2020-09-02 15:21:51 -0400

    Moved Examples to SDK v1 Examples. Created SDK v2 Examples folder and made a quickstart project. Project does nothing yet

---
+++ SDK v2 Examples/quickstart/quickstart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quickstart
+//
+//  Created by Youel Kaisar on 2020-09-02.
+//  Copyright © 2020 Mappedin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
