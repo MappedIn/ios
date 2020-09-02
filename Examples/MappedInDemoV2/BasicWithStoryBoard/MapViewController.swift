00f0cb24fe96a066c7003d793572733c74eb3881
youel <youel.kaisar@mappedin.ca>
2020-09-02 13:58:45 -0400

    Added comments and refactored to stay similar to the Quick Start

--- Examples/MappedInDemoV2/BasicWithStoryBoard/MapViewController.swift
+++ Examples/MappedInDemoV2/BasicWithStoryBoard/MapViewController.swift
@@ -12,7 +12,6 @@ import Mappedin
 
 class MapViewController: UIViewController{
     
-    var venueSlug: String = "mappedin-demo-mall"
     var venue:MiVenue!
 
     var mapView: MiMapView!
@@ -30,17 +29,36 @@ class MapViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create a Mappedin object and enter your keys through the property list file or the initializer
         let mappedIn = Mappedin()
+        
+        //
         let customStyleURL = Bundle.main.url(forResource: "third_party_style", withExtension: "json")!
+        
+        // Initialize an MiMapView object with the viewport bounds to contain the map
+        // and the Mapbox style URL, if any.
         mapView = MiMapView(frame: view.bounds, styleURL: customStyleURL)
+        
+        // Sets the MiMapViewDelegate
+        mapView.miDelegate = self
+
+        // Add the MiMapView as a subview of the controller's view.
         view.addSubview(mapView)
         view.sendSubviewToBack(mapView)
-        mapView.miDelegate = self
         
-        
-        mappedIn.getVenue(venueSlug: venueSlug, completionHandler: {(resultCode, venue) -> Void in
-            self.venue = venue
-            self.mapView.loadMap(venue: venue!)
+        // Use the retrieved venue
+        mappedIn.getVenue(venueSlug: "mappedin-demo-mall", completionHandler: {(resultCode, venue) -> Void in
+            if resultCode == MIGetVenueResultCode.SUCCESS {
+                // This venue object contains all of the location, obstruction, space, category data required to display the map
+                if let venue = venue {
+                    self.venue = venue
+                    
+                    // With this venue object, you can now load it into the map as seen below:
+                    self.mapView.loadMap(venue: venue)
+                }
+            }
+            
         })
     }
 
@@ -59,30 +77,40 @@ class MapViewController: UIViewController{
 
 extension MapViewController: MiMapViewDelegate {
     func onTapNothing() {
-        
+        // Called when a tap doesn't hit any spaces
     }
     
     func didTapSpace(space: MiSpace) -> Bool {
+        // Called when a space is tapped and provides which space was tapped
+        // Return true to stop the tap
+        // Return false to allow the tap to fall through and trigger other events
         return false
     }
     
     func onTapCoordinates(point: CLLocationCoordinate2D) {
-
+        // Called when the map is tapped and provides the coordinates of the tap
     }
     
     func didTapOverlay(overlay: MiOverlay) -> Bool {
+        // Called when an overlay is tapped and provides which overlay was tapped
+        // Return true to stop the tap
+        // Return false to allow the tap to fall through and trigger other events
         return false
     }
     
     func onLevelChange(level: MiLevel) {
-        mapView.focusOnCurrentLevel(padding: 50)
+        // Called when the level is changed and provides the new level
     }
     
     func onManipulateCamera(gesture: MiGestureType) {
-        
+        // Called when the user manipulates the camera
     }
     
+    
     func onMapLoaded(status: MiMapStatus) {
+        // Called when the `MiMapView` has finished loading both the view and the venue data
+        
+        // Makes the mapView focus on the current levels coordinates
         self.mapView.focusOnCurrentLevel()
     }
 }
