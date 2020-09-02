02f932d188c1872814deca0eb6767914a1a4602b
youel <youel.kaisar@mappedin.ca>
2020-09-02 15:54:06 -0400

    Added quickstart project

--- SDK v2 Examples/quickstart/quickstart/ViewController.swift
+++ SDK v2 Examples/quickstart/quickstart/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Mappedin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create a Mappedin object and enter your keys through the initializer
+        let mappedIn = Mappedin(mappedinKey: "5f2c5b9aa5fdf5001a6b971d", mappedinSecret: "tsKYDtd9xnks2WNu9YHS5SrfHwwENGeHnaAyMTI5jPhpsEbU")
+        
+        let mapView = MiMapView(frame: view.bounds, styleURL: nil)
+        view.addSubview(mapView)
+        
+        // get the venue
+        // The completionHandler closure is called once the venue has finished loading
+        // The completion handler passes an MiGetVenueResultCode on whether or not the venue data was retrieved
+        mappedIn.getVenue(venueSlug: "mappedin-demo-mall") { (result, venue) in
+            // This venue object contains all of the location, obstruction, space, category data required to display the map
+            if let venue = venue {
+                // With this venue object, you can now load it into the map as seen below:
+                mapView.loadMap(venue: venue)
+            }
+        }
     }
-
-
 }
 
