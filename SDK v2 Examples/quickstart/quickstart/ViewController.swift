23616e2c03d296e0563c5d4fe8e1b963a4846abd
Christine Maiolo <maiolo.c313@gmail.com>
2020-09-15 17:40:18 -0400

    Updated the pod file and added some comments to the quickstart

--- SDK v2 Examples/quickstart/quickstart/ViewController.swift
+++ SDK v2 Examples/quickstart/quickstart/ViewController.swift
@@ -15,18 +15,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Create a Mappedin object and enter your keys through the initializer
-        let mappedIn = Mappedin(mappedinKey: "5f4e59bb91b055001a68e9d9", mappedinSecret: "gmwQbwuNv7cvDYggcYl4cMa5c7n0vh4vqNQEkoyLRuJ4vU42")
-        
+        // You can use these sample keys for now to access a few demo venues
+        let mappedin = Mappedin(mappedinKey: "5f4e59bb91b055001a68e9d9", mappedinSecret: "gmwQbwuNv7cvDYggcYl4cMa5c7n0vh4vqNQEkoyLRuJ4vU42")
+
+        // Set up the MapView, this is where you will render your maps
         let mapView = MiMapView(frame: view.bounds, styleURL: nil)
         view.addSubview(mapView)
         
-        // get the venue
-        // The completionHandler closure is called once the venue has finished loading
-        // The completion handler passes an MiGetVenueResultCode on whether or not the venue data was retrieved
-        mappedIn.getVenue(venueSlug: "mappedin-demo-mall") { (result, venue) in
-            // This venue object contains all of the location, obstruction, space, category data required to display the map
+        // Get the venue:
+        // With the sample keys provided above, you have access to mappedin-demo-mall and a
+        // few other demo venues. You can access the complete list with mappedin.getVenues()
+        // For now we'll just load mappedin-demo-mall
+        mappedin.getVenue(venueSlug: "mappedin-demo-mall") { (result, venue) in
+            // The completionHandler closure is called once the venue has finished loading
+            // The completion handler passes an MiGetVenueResultCode on whether or not the
+            // venue data was retrieved
             if let venue = venue {
-                // With this venue object, you can now load it into the map as seen below:
+                // Load the venue into the mapView
+                // This venue object contains all of the location, obstruction, space, category data
                 mapView.loadMap(venue: venue)
             }
         }
