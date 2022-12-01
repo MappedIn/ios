e9e0d6fe45df84dfabf29a458bebb65a29f3e615
Zach Merrill <zachary.merrill@mappedin.ca>
2022-12-01 11:39:45 -0500

    Migrate v4 examples to v5

---
+++ SDK v5 Examples/ios-sdk-app/ios-sdk-app/ViewController+UIPickerViewDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController+UIPickerViewDelegate.swift
+//  ios-sdk-app
+//
+
+import UIKit
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return mapView?.venueData?.maps.count ?? 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return mapView?.venueData?.maps[row].name ?? ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if let selectedMap = mapView?.venueData?.maps[row] {
+            mapView?.setMap(map: selectedMap)
+        }
+    }
+    
+}
