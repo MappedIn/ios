520ed7c69e0083915d022a7931f59973615e78c1
Zach Merrill <zachary.merrill@mappedin.ca>
2023-01-18 11:53:28 -0500

    Add search example

--- SDK v5 Examples/PlaygroundSamples/PlaygroundSamples/Examples/SearchVC.swift
+++ SDK v5 Examples/PlaygroundSamples/PlaygroundSamples/Examples/SearchVC.swift
@@ -6,12 +6,17 @@
 import Mappedin
 import UIKit
 
-class SearchVC: UIViewController {
+class SearchVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, MPIMapViewDelegate {
+    let searchStackView = UIStackView()
+    let tableView = UITableView()
+    let search = UISearchBar()
+    var results: [MPILocation] = .init()
     var mapView: MPIMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView = MPIMapView(frame: view.frame)
+        view.backgroundColor = .systemBackground
+        mapView = MPIMapView(frame: CGRect(x: 0, y: view.frame.height / 4, width: view.frame.width, height: (view.frame.height / 4) * 3))
         mapView?.delegate = self
         if let mapView = mapView {
             view.addSubview(mapView)
@@ -23,17 +28,64 @@ class SearchVC: UIViewController {
                     clientId: "5eab30aa91b055001a68e996",
                     clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1",
                     venue: "mappedin-demo-mall"
-                ),
-                showVenueOptions: MPIOptions.ShowVenue(
-                    labelAllLocationsOnInit: false,
-                    backgroundColor: "#ffffff"
                 ))
         }
+        setupSearchView()
     }
-}
+    
+    func setupSearchView() {
+        view.addSubview(searchStackView)
+        searchStackView.axis = .vertical
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.heightAnchor.constraint(equalToConstant: view.frame.height / 4).isActive = true
+        searchStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        searchStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
 
-extension SearchVC: MPIMapViewDelegate {
-    func onDataLoaded(data: Mappedin.MPIData) {}
+        search.placeholder = "Search..."
+        search.delegate = self
+        searchStackView.addArrangedSubview(search)
+        
+        tableView.register(LocationCell.self, forCellReuseIdentifier: LocationCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchStackView.addArrangedSubview(tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: LocationCell.identifier, for: indexPath) as! LocationCell
+        cell.nameLabel.text = results[indexPath.row].name
+        cell.descLabel.text = results[indexPath.row].description
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        mapView?.searchManager.search(query: searchText) {
+            searchResult in
+            var filteredSearchResults: [MPILocation] = .init()
+            let matches = searchResult.flatMap { $0.matches.filter { $0.matchesOn == "name" } }
+            for match in matches {
+                if let location = self.mapView?.venueData?.locations.first(where: { $0.name == match.value }) {
+                    // Check for duplicates
+                    guard !filteredSearchResults.contains(where: { $0.name == location.name }) else {
+                        continue
+                    }
+                    filteredSearchResults.append(location)
+                }
+            }
+            self.results = filteredSearchResults
+            self.tableView.reloadData()
+        }
+    }
+    
+    func onDataLoaded(data: Mappedin.MPIData) {
+        results = mapView?.venueData?.locations ?? [MPILocation]()
+        tableView.reloadData()
+    }
     
     func onFirstMapLoaded() {}
     
@@ -51,4 +103,3 @@ extension SearchVC: MPIMapViewDelegate {
     
     func onPolygonClicked(polygon: MPIPolygon) {}
 }
-
