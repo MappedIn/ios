659486cd3c59a2d0a971e7577ce3a96e528c3be5
Mark Sohm <MSohm@users.noreply.github.com>
2023-10-10 11:10:08 -0400

    Migrate sample to use MPIMapClickDelegate and enable XRayPaths (#59)
    * Migrate sample to use MPIMapClickDelegate and Enable XrayPaths
    
    * Add new interactive objects to sample
    
    Update for Mappedin SDK for iOS 5.1.8

--- SDK v5 Examples/PlaygroundSamples/PlaygroundSamples/Examples/AddingInteractivityVC.swift
+++ SDK v5 Examples/PlaygroundSamples/PlaygroundSamples/Examples/AddingInteractivityVC.swift
@@ -6,12 +6,16 @@
 import Mappedin
 import UIKit
 
-class AddingInteractivityVC: UIViewController, MPIMapViewDelegate {
+class AddingInteractivityVC: UIViewController, MPIMapClickDelegate, MPIMapViewDelegate {
+
+    
+
     var mapView: MPIMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView = MPIMapView(frame: view.frame)
+        mapView?.mapClickDelegate = self
         mapView?.delegate = self
         if let mapView = mapView {
             view.addSubview(mapView)
@@ -27,11 +31,75 @@ class AddingInteractivityVC: UIViewController, MPIMapViewDelegate {
         }
     }
     
+    func onClick(mapClickEvent: Mappedin.MPIMapClickEvent) {
+        var message = ""
+        
+        // Use the map name as the title.
+        let title = mapClickEvent.maps.first?.name ?? ""
+        
+        // If a floating label was clicked, add its text to the message.
+        if (!mapClickEvent.floatingLabels.isEmpty)  {
+            message.append("Floating Label Clicked: ")
+            message.append(mapClickEvent.floatingLabels.first?.text ?? "")
+            message.append("\n")
+        }
+        
+        // If a polygon was clicked, add it's location name to the message.
+        if (!mapClickEvent.polygons.isEmpty) {
+            message.append("Polygon clicked: ")
+            message.append(mapClickEvent.polygons.first?.locations?.first?.name ?? "")
+            message.append("\n")
+        }
+
+        // If a path was clicked, add it to the message.
+        if (!mapClickEvent.paths.isEmpty) {
+            message.append("You clicked a path.\n")
+        }
+        
+        // Add the coordinates clicked to the message.
+        message.append("Coordinate Clicked: \nLatitude: ")
+        message.append(mapClickEvent.position?.latitude.description ?? "")
+        message.append("\nLongitude: ")
+        message.append(mapClickEvent.position?.longitude.description ?? "")
+        
+        showMessage(title: title, message: message)
+        
+    }
+    
     func onDataLoaded(data: Mappedin.MPIData) {}
     
-    func onFirstMapLoaded() {}
+    func onFirstMapLoaded() {
+        // Make floating labels interactive.
+        mapView?.floatingLabelManager.labelAllLocations(options: MPIOptions.FloatingLabelAllLocations(interactive: true))
+        
+        // Draw an interactive journey.
+        var departure = mapView?.venueData?.locations.first(where: {$0.name == "Microsoft"} )
+        var destination = mapView?.venueData?.locations.first(where: {$0.name == "Apple"} )
+        
+        if (departure == nil || destination == nil ) {return}
+        
+        let journeyOpt = MPIOptions.Journey(pathOptions: MPIOptions.Path(interactive: true))
+        
+        mapView?.getDirections(to: destination!, from: departure!) {
+            directions in
+            self.mapView?.journeyManager.draw(directions: directions!, options: journeyOpt)
+        }
+        
+        //Draw an interactive path.
+        departure = mapView?.venueData?.locations.first(where: {$0.name == "Uniqlo"} )
+        destination = mapView?.venueData?.locations.first(where: {$0.name == "Nespresso"} )
+        
+        if (departure == nil || destination == nil ) {return}
+        
+        mapView?.getDirections(to: destination!, from: departure!) {
+            directions in
+            self.mapView?.pathManager.add(nodes: directions!.path, options: MPIOptions.Path(interactive: true))
+        }
+    }
     
     func onMapChanged(map: Mappedin.MPIMap) {}
+    
+    func onPolygonClicked(polygon: Mappedin.MPIPolygon) {}
     
     func onNothingClicked() {}
     
@@ -43,7 +111,10 @@ class AddingInteractivityVC: UIViewController, MPIMapViewDelegate {
     
     func onCameraChanged(cameraChange: Mappedin.MPICameraTransform) {}
     
-    func onPolygonClicked(polygon: MPIPolygon) {
-        mapView?.cameraManager.focusOn(targets: MPIOptions.CameraTargets(polygons: [polygon]))
+    private func showMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
+    
 }
