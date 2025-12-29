4b86c0c027aba8fb3b6fbe7431d97733f0afb43b
Mark Sohm <mark.sohm@mappedin.ca>
2025-12-29 16:00:15 -0500

    add camera transform logging

--- SDK v6 Examples/PlaygroundSamples/PlaygroundSamples/CameraDemoViewController.swift
+++ SDK v6 Examples/PlaygroundSamples/PlaygroundSamples/CameraDemoViewController.swift
@@ -237,6 +237,14 @@ final class CameraDemoViewController: UIViewController {
 				self.mapView.camera.focusOn(coordinate: click.coordinate)
 			}
 		}
+        
+        // Log camera change events to the console.
+        mapView.on("camera-change") { [weak self] payload in
+            guard self != nil else { return }
+            if let cameraTransform = payload as? CameraTransform {
+                print("Camera changed to bearing: \(cameraTransform.bearing), pitch: \(cameraTransform.pitch), zoomLevel: \(cameraTransform.zoomLevel), center: Lat: \(cameraTransform.center.latitude), Lon: \(cameraTransform.center.longitude)")
+            }
+        }
     }
 }
 
